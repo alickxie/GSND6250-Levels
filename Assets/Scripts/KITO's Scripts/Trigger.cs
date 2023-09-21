4d31c284b9cf8b48340b23f21e40b441e48e5041
XiangxiiZhang <75644979+XiangxiiZhang@users.noreply.github.com>
2023-09-21 13:32:27 -0400

    Kito scene, ring trigger in certain range

--- Assets/Scripts/KITO's Scripts/Trigger.cs
+++ Assets/Scripts/KITO's Scripts/Trigger.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Trigger : MonoBehaviour
 {
     [SerializeField] private bool triggerActive = false;
+    private AudioSource audioSource;
+    public float activationRange = 18.0f;
+    private bool hasPlayed = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -19,13 +27,27 @@ public class Trigger : MonoBehaviour
             triggerActive = false;
         }
     }
-
     private void Update()
     {
-        if (triggerActive && Input.GetKeyDown(KeyCode.Space))
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
         {
-            print("yeah");
-            GetComponent<AudioSource>().Stop();
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            Debug.Log("Distance to player: " + distance);
+            // Play the audio if the player is in range and the audio is not already playing
+            if (distance <= activationRange && !audioSource.isPlaying && hasPlayed == false)
+            {
+                Debug.Log("Audio should start playing.");
+                audioSource.Play();
+                hasPlayed = true;
+                Debug.Log("Audio started playing.");
+            }
+            // Stop the audio if the player is in range and spacebar is pressed
+            else if (distance <= activationRange && Input.GetKeyDown(KeyCode.Space) && audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
         }
     }
+
 }
