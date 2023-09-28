ad0e5c1b4869dea0e9f45397c446626db06ecd9b
XiangxiiZhang <75644979+XiangxiiZhang@users.noreply.github.com>
2023-09-27 23:29:50 -0400

    button

---
+++ Level_Tension/Assets/Scripes/KITO Scripe/ButtonPush.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPush : MonoBehaviour
+{
+    public Transform camera;
+    public float playerActivateDistance = 5f;
+    bool active = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        active = Physics.Raycast(camera.position, camera.forward, out hit, playerActivateDistance);
+
+        if (Input.GetKeyDown(KeyCode.E) && active)
+        {
+            if (hit.transform.gameObject != null)
+            {
+                // get the hit object name
+                if (hit.transform.gameObject.name == "Switch1")
+                {
+                    Debug.Log("Switch1!");
+                }
+                else if (hit.transform.gameObject.name == "Switch2")
+                {
+                    Debug.Log("Switch2!");
+                }
+            }
+        }
+    }
+}
