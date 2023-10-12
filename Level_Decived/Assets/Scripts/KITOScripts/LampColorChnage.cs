d90fc0ce71e5f195d86e0f8c32579210b486ac5d
XiangxiiZhang <75644979+XiangxiiZhang@users.noreply.github.com>
2023-10-12 15:04:05 -0400

    lamp color circle

--- Level_Decived/Assets/Scripts/KITOScripts/LampColorChnage.cs
+++ Level_Decived/Assets/Scripts/KITOScripts/LampColorChnage.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class LampColorChnage : MonoBehaviour
 {
     public Material glassMaterial;  // Reference to the glass material
-    private Color originalColor;   // Store the original color of the material
+    private Color[] colors;        // Array to hold the colors
+    private int currentColorIndex;  // Index to keep track of the current color
 
     // Start is called before the first frame update
     void Start()
     {
-        // Store the original color of the material 
-        //E7021E
-        originalColor = glassMaterial.color;
+        // Store the original color of the material
+        colors = new Color[] {
+            Color.red,
+            Color.green,
+            Color.blue
+        };
+
+        // Initialize the current color index to 0 (first color in the array)
+        currentColorIndex = 0;
+
+        glassMaterial.color = colors[currentColorIndex];
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,21 +30,21 @@ public class LampColorChnage : MonoBehaviour
         {
             Debug.Log("Player entered the trigger.");
 
-            // Change the tint color to a random color
-            Color newColor = new Color(Random.value, Random.value, Random.value);
-            glassMaterial.color = newColor;
+            // Cycle to the next color
+            currentColorIndex = (currentColorIndex + 1) % colors.Length;
+            glassMaterial.color = colors[currentColorIndex];
         }
     }
-/*
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
+    /*
+        private void OnTriggerExit(Collider other)
         {
-            Debug.Log("Player exited the trigger.");
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("Player exited the trigger.");
 
-            // Restore the original color of the material
-            glassMaterial.color = originalColor;
+                // Restore the original color of the material
+                glassMaterial.color = originalColor;
+            }
         }
-    }
-*/
+    */
 }
---
+++ Level_Decived/Assets/Scripts/KITOScripts/LampColorChnage1.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampColorChnage1 : MonoBehaviour
+{
+    public Material glassMaterial;  // Reference to the glass material
+    private Color[] colors;        // Array to hold the colors
+    private int currentColorIndex;  // Index to keep track of the current color
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Store the original color of the material
+        colors = new Color[] {
+            Color.red,
+            Color.green,
+            Color.blue
+        };
+
+        // Initialize the current color index to 0 (first color in the array)
+        currentColorIndex = 0;
+
+        glassMaterial.color = colors[currentColorIndex];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered the trigger.");
+
+            // Cycle to the next color
+            currentColorIndex = (currentColorIndex + 1) % colors.Length;
+            glassMaterial.color = colors[currentColorIndex];
+        }
+    }
+    /*
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("Player exited the trigger.");
+
+                // Restore the original color of the material
+                glassMaterial.color = originalColor;
+            }
+        }
+    */
+}
