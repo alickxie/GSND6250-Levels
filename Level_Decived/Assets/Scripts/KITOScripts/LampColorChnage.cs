06ab954c8befaa61fcffa044a28d5edbd348031e
XiangxiiZhang <75644979+XiangxiiZhang@users.noreply.github.com>
2023-10-12 14:37:15 -0400

    Update LampColorChnage.cs

--- Level_Decived/Assets/Scripts/KITOScripts/LampColorChnage.cs
+++ Level_Decived/Assets/Scripts/KITOScripts/LampColorChnage.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class LampColorChnage : MonoBehaviour
 {
+    public Material glassMaterial;  // Reference to the glass material
+    private Color originalColor;   // Store the original color of the material
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start");
+        // Store the original color of the material 
+        //E7021E
+        originalColor = glassMaterial.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-
-    }
-
-    void OnCollisionEnter(Collision other)
-    {
-        if (collision.gameObject.CompareTag("Player")) 
+        if (other.CompareTag("Player"))
         {
-            // Change the material color to the new material's color.
-            //renderer.material = newMaterial;
-            Debug.Log("Enter");
+            Debug.Log("Player entered the trigger.");
+
+            // Change the tint color to a random color
+            Color newColor = new Color(Random.value, Random.value, Random.value);
+            glassMaterial.color = newColor;
         }
     }
+/*
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited the trigger.");
+
+            // Restore the original color of the material
+            glassMaterial.color = originalColor;
+        }
+    }
+*/
 }
