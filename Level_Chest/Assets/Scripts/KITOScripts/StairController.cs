28b332fa7781f5590253bcffc7adc6703923e938
XiangxiiZhang <75644979+XiangxiiZhang@users.noreply.github.com>
2023-10-19 17:14:53 -0400

    starirs

--- Level_Chest/Assets/Scripts/KITOScripts/StairController.cs
+++ Level_Chest/Assets/Scripts/KITOScripts/StairController.cs
@@ -1,44 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StairController : MonoBehaviour
 {
     public GameObject[] stairCubes;
-    public float raiseSpeed = 1.0f; // Adjust the speed as needed
-    private float[] targetHeights;
     private bool stairsRaised = false;
-
-    public void Start()
-    {
-        targetHeights = new float[stairCubes.Length];
-        for (int i = 0; i < stairCubes.Length; i++)
-        {
-            targetHeights[i] = stairCubes[i].transform.position.y + i; // Adjust the raise offset
-        }
-    }
-
-    public void Update()
-    {
-        if (!stairsRaised)
-        {
-            RaiseStairs();
-        }
-    }
+    private int currentCubeIndex = 0;
 
     public void RaiseStairs()
     {
-        for (int i = 0; i < stairCubes.Length; i++)
+        if (currentCubeIndex < stairCubes.Length)
         {
-            float step = raiseSpeed * Time.deltaTime;
-            stairCubes[i].transform.position = Vector3.MoveTowards(stairCubes[i].transform.position, new Vector3(stairCubes[i].transform.position.x, targetHeights[i], stairCubes[i].transform.position.z), step);
+            // Move the current cube up (you can use Translate or other methods)
+            float raiseAmount = 1.0f; // Adjust this value as needed
+            stairCubes[currentCubeIndex].transform.Translate(Vector3.up * raiseAmount);
 
-            if (stairCubes[i].transform.position.y >= targetHeights[i])
+            currentCubeIndex++;
+
+            if (currentCubeIndex == stairCubes.Length)
             {
                 stairsRaised = true;
             }
-            Debug.Log($"Stair {i} Position: {stairCubes[i].transform.position.y} Target Height: {targetHeights[i]}");
         }
     }
 }
-
