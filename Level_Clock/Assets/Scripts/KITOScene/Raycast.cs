52458885634a7ed9a82ede12534e62bf1080720e
XiangxiiZhang <75644979+XiangxiiZhang@users.noreply.github.com>
2023-10-26 12:27:17 -0400

    pickup item

--- Level_Clock/Assets/Scripts/KITOScene/Raycast.cs
+++ Level_Clock/Assets/Scripts/KITOScene/Raycast.cs
@@ -6,17 +6,23 @@ using UnityEngine.UI;
 public class Raycast : MonoBehaviour
 {
     [Header("Raycast Features")]
-    [SerializeField] private float rayLength = 5;
+    [SerializeField] private float rayLength;
     private Camera _camera;
     
     private NoteController _noteController;
-    
+
     [Header("Crosshair")]
     [SerializeField] private Image crosshair;
 
     [Header("Input Key")]
     [SerializeField] private KeyCode interactKey;
 
+    //add
+    [Header("Holding Position")]
+    [SerializeField] private Transform HoldingPosition;
+    [SerializeField] private LayerMask PickupMask;
+    private Rigidbody CurrentObject;
+    private int _pickupController;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +35,21 @@ public class Raycast : MonoBehaviour
     {
         if (Physics.Raycast(_camera.ViewportToWorldPoint(new Vector3(0.5f,0.5f)),transform.forward, out RaycastHit hit, rayLength))
         {
+            int pickupableItem = hit.collider.gameObject.layer;
+            //Debug.Log(pickupableItem);
+            if(pickupableItem == 18)
+            {
+                _pickupController = pickupableItem;
+                HeighlightCrosshair(true);
+            }
+            else
+            {
+                ClearPickup();
+            }
+
+
             var readableItem = hit.collider.GetComponent<NoteController>();
+            //Debug.Log(readableItem);
             if (readableItem != null)
             {
                 _noteController = readableItem;
@@ -43,21 +63,63 @@ public class Raycast : MonoBehaviour
         else
         {
             ClearNote();
+            ClearPickup();
         }
-        if(_noteController != null)
+        if (_noteController != null)
         {
             if (Input.GetKeyDown(interactKey))
             {
                 _noteController.ShowNote();
             }
         }
+        if(_pickupController == 18)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                if (CurrentObject)
+                {
+                    CurrentObject.useGravity = true;
+                    CurrentObject = null;
+                    return;
+                }
+                CurrentObject = hit.rigidbody;
+                CurrentObject.useGravity = false;
+            }
+        }
+        //add
+
+        /*
+        if (Input.GetKeyDown(interactKey))
+        {
+            if (CurrentObject)
+            {
+                CurrentObject.useGravity = true;
+                CurrentObject = null;
+                return;
+            }
+            if (Physics.Raycast(_camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f)), transform.forward, out RaycastHit hitInfo, rayLength, PickupMask))
+            {
+                CurrentObject = hitInfo.rigidbody;
+                CurrentObject.useGravity = false;
+            }
+        }*/
+
     }
+
     void ClearNote()
     {
         if (_noteController != null)
         {
             HeighlightCrosshair(false);
             _noteController = null;
+        }
+    }
+    void ClearPickup()
+    {
+        if(_pickupController == 18 )
+        {
+            HeighlightCrosshair(false);
+            _pickupController = 0;
         }
     }
 
@@ -70,6 +132,18 @@ public class Raycast : MonoBehaviour
         else
         {
             crosshair.color = Color.white;
+        }
+    }
+
+    //add
+    void FixedUpdate()
+    {
+        if (CurrentObject)
+        {
+            Vector3 DirectionToPoint = HoldingPosition.position - CurrentObject.position;
+            float DistanceToPoint = DirectionToPoint.magnitude;
+
+            CurrentObject.velocity = DirectionToPoint * 12f * DistanceToPoint;
         }
     }
 }
