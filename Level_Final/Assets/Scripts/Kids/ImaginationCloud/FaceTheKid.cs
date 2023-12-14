746b8f8caf44880e673e305d76dfff5c1bf71ffb
XiangxiiZhang <75644979+XiangxiiZhang@users.noreply.github.com>
2023-12-14 14:37:02 -0500

    image disappear

--- Level_Final/Assets/Scripts/Kids/ImaginationCloud/FaceTheKid.cs
+++ Level_Final/Assets/Scripts/Kids/ImaginationCloud/FaceTheKid.cs
@@ -7,11 +7,13 @@ public class FaceTheKid : MonoBehaviour
     public Transform kid;
     public float range;
     public Animator animator;
+    public GameObject decorationOnHand;
+    [SerializeField] private string itemName;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -30,6 +32,11 @@ public class FaceTheKid : MonoBehaviour
         {
             animator.SetBool("leave", true);
         }
+        if (decorationOnHand != null)
+        {
+            CompareItems();
+        }
+
 
     }
 
@@ -38,5 +45,25 @@ public class FaceTheKid : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    public void CompareItems()
+    {
+        // get first child of decorationOnTree
+        if (decorationOnHand.transform.childCount > 0)
+        {
+            // Debug.Log("Child count: " + decorationOnHand.transform.childCount);
+            GameObject child = decorationOnHand.transform.GetChild(0).gameObject;
+            if (itemName == child.GetComponent<HoldInHand>().GetItemName())
+            {
+                animator.SetBool("leave", true);
+                Invoke("DeactivateObject", 1f); // Schedule deactivation after 1 second
+            }
+        }
+    }
+
+    public void DeactivateObject()
+    {
+        this.gameObject.SetActive(false);
     }
 }
