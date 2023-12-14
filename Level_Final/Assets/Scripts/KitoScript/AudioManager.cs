d4806f84bc392a406a953813e531438884bc13d6
XiangxiiZhang <75644979+XiangxiiZhang@users.noreply.github.com>
2023-12-14 04:52:40 -0500

    generative window Rattle sfx

--- Level_Final/Assets/Scripts/KitoScript/AudioManager.cs
+++ Level_Final/Assets/Scripts/KitoScript/AudioManager.cs
@@ -8,6 +8,10 @@ public class AudioManager : MonoBehaviour
 
     private AudioSource audioSource;
 
+    public AudioClip sound1;
+    public AudioClip sound2;
+    public AudioClip sound3;
+
     void Awake()
     {
         // Singleton pattern to ensure only one instance of AudioManager exists
@@ -31,4 +35,39 @@ public class AudioManager : MonoBehaviour
             audioSource.PlayOneShot(clip);
         }
     }
+
+    public void StartPlayingSound1()
+    {
+        StartCoroutine(PlaySoundWithDelay(sound1, 5f, 0.33f));
+    }
+
+    public void StartPlayingSound2()
+    {
+        StartCoroutine(PlaySoundWithDelay(sound2, 3.3f, 0.10f));
+    }
+
+    public void StartPlayingSound3()
+    {
+        StartCoroutine(PlaySoundWithDelay(sound3, 7.4f, 0.50f));
+    }
+
+    IEnumerator PlaySoundWithDelay(AudioClip clip, float delay, float chance)
+    {
+        while (true) // Infinite loop to keep checking
+        {
+            yield return new WaitForSeconds(delay);
+
+            if (Random.value < chance)
+            {
+                audioSource.PlayOneShot(clip);
+            }
+        }
+    }
+    void Start()
+    {
+        StartPlayingSound1();
+        StartPlayingSound2();
+        StartPlayingSound3();
+    }
+
 }
